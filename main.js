272ff5e4f16cb9d9d93a91d167c6062a66e87a4e
Riza Marcos <marcossriza@gmail.com>
2022-06-24 21:47:15 +0800

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+const num1 = parseFloat(prompt('Enter number 1:'));
+const num2 = parseFloat(prompt('Enter number 2:'));
+
+
+const operator = prompt('Enter operator(+,-,/,*');
+let result = 0;
+if(isNaN(num1) || isNaN(num2)){
+    alert('Wrong Input!')
+}else{
+    if(operator== '+'){
+        result = num1 + num2;
+    }else if(operator == '-'){
+        result = num1 - num2;
+    }else if(operator == '/'){
+        result = num1 / num2;
+    }else if(operator == '*'){
+        result = num1 * num2;
+    }
+    alert(num1+operator+num2+'=' + result);
+}
